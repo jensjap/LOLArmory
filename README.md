3a28ba83cdaa8d4b96f2dcf9fc0a2e99612228dc
Jens Jap <illiteratepuppy@gmail.com>
2012-09-26 21:31:57 -0700

    built first function and started building wrapper to access MySQL

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 LOLArmory
 =========
 
+Contributors:
+Stephanie
+Daniel Rodriguez
+Matthew Campbell
+Kenjy Jap
+Jens Jap
+
+Description:
 Build web application to query League of Legends database for character information
---
+++ jens.py
@@ -0,0 +1,27 @@
+def buildInfoRetrievalSQLQuery(s_charName, i_infoSelection):
+    """ usage: buildInfoRetrievalSQLQuery(Character Name, Info Selection)
+
+        Function requires two paramenters. It will build the 
+        query to retrieve the selected information based on the 
+        Character Name and Info Selection
+
+        return SQL query string """
+
+    d_infoSelection = {
+        1: "character_description",
+        2: "character_stats"
+    }
+
+    s_sqlQuery = "SELECT %s FROM LOLArmory.character_names WHERE \
+character_name = '%s'" % (d_infoSelection[i_infoSelection],
+                        s_charName)
+
+    return s_sqlQuery
+
+
+# ### Unit TEST ###
+# s_charName      = 'jens'
+# i_infoSelection = 2
+
+# sqlStatement = buildInfoRetrievalSQLQuery(s_charName, i_infoSelection)
+# print(sqlStatement)
